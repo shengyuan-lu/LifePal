40cc20d6e7383f2a1b0c32f0e291a52e7ba01641
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-04-17 10:15:39 -0700

    Initial Commit

---
+++ LifePal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LifePal
+//
+//  Created by Shengyuan Lu on 4/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifePal/LifePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifePalApp.swift
+//  LifePal
+//
+//  Created by Shengyuan Lu on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LifePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
