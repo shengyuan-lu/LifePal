9a9fadb58c4dcdddde4dc63c8cebfe7092c0b51b
arkinator1000 <arkinjai@gmail.com>
2023-05-27 19:43:57 -0700

    Changed parameters for API links

--- LifePal/Helpers/Links.swift
+++ LifePal/Helpers/Links.swift
@@ -10,8 +10,10 @@ import Foundation
 struct Links {
     
     // Remote Real API Endpoints
-    static let menuRecommendationAPI = "http://3.15.22.210:8000/api/recommendfood?weight=$weight$&bodyfat=$bodyfat$&avg_activity=$avg_activity$&cb=2&cl=2&cd=2"
+    static let menuRecommendationAPI = "http://3.15.22.210:8000/api/foodrec/?weight=$weight$&bodyfat=$bodyfat$&avg_activity=$avg_activity$&cb=2&cl=2&cd=2"
     static let fullMenuAPI = "http://3.15.22.210:8000/api/menu"
+    static let waterRecommendationAPI =
+        "http://3.15.22.210:8000/api/waterrec/?age=$age$&weight=$weight$&height=$height$&avg_activity=$avg_activity$&temperature=$temperature$"
     
     // Remote Demo API Endpoints
     static let menuRecommendationAPISample = "http://3.15.22.210:8000/api/recommendfood?weight=55&bodyfat=0.165&avg_activity=300"
