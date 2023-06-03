0a920fc59ba89b51eb3bc33ea58a3ce0e4a723f0
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-06-02 21:22:56 -0700

    add login functionalities

--- LifePal/LifePalApp.swift
+++ LifePal/LifePalApp.swift
@@ -14,20 +14,29 @@ struct LifePalApp: App {
     @StateObject var recommendedMenu: MenuVM = MenuVM(isRecommeded: true)
     @StateObject var fullMenu: MenuVM = MenuVM(isRecommeded: false)
     
+    @State var isLoggedin = false
+    
     var body: some Scene {
         WindowGroup {
-            MainView()
-                .onAppear {
-                    // correct the transparency bug for Tab bars
-                    let tabBarAppearance = UITabBarAppearance()
-                    tabBarAppearance.configureWithOpaqueBackground()
-                    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-                    
-                    // correct the transparency bug for Navigation bars
-                    let navigationBarAppearance = UINavigationBarAppearance()
-                    navigationBarAppearance.configureWithOpaqueBackground()
-                    UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
-                }
+            
+            if isLoggedin {
+                MainView()
+                    .onAppear {
+                        // correct the transparency bug for Tab bars
+                        let tabBarAppearance = UITabBarAppearance()
+                        tabBarAppearance.configureWithOpaqueBackground()
+                        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                        
+                        // correct the transparency bug for Navigation bars
+                        let navigationBarAppearance = UINavigationBarAppearance()
+                        navigationBarAppearance.configureWithOpaqueBackground()
+                        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+                    }
+            } else {
+                LoginView(isLoggedin: $isLoggedin)
+            }
+            
+
         }
     }
 }
