9ab1e7924a15766c8b3c0ce9a7fb8c0ad0550c3d
arkinator1000 <arkinjai@gmail.com>
2023-06-04 18:10:03 -0700

    small syntax error (missing })

--- LifePal/Views/SleepViews/SleepView.swift
+++ LifePal/Views/SleepViews/SleepView.swift
@@ -12,50 +12,51 @@ struct SleepView: View {
     @ObservedObject var healthVM: HealthVM
     @ObservedObject var sleepVM: SleepVM
     
-//    @State private var wakeUpTime = Date()
-//    @State private var recommendedBedTime = ""
-
-//    private var bedtimeCalculator: BedtimeCalculator {
-//            BedtimeCalculator()
-//        }
-
-        var body: some View {
+    //    @State private var wakeUpTime = Date()
+    //    @State private var recommendedBedTime = ""
+    
+    //    private var bedtimeCalculator: BedtimeCalculator {
+    //            BedtimeCalculator()
+    //        }
+    
+    var body: some View {
+        VStack {
+            Text("Sleep Calculator")
+                .font(.largeTitle)
+                .padding(.top, 50) // Add top padding to title
+            
+            Spacer()
+            
             VStack {
-                        Text("Sleep Calculator")
-                            .font(.largeTitle)
-                            .padding(.top, 50) // Add top padding to title
-
-                        Spacer()
-
-                        VStack {
-                            Text("Wake Up Time")
-                                .font(.headline)
-
-                            DatePicker("", selection: $sleepVM.wakeUpTime, displayedComponents: .hourAndMinute)
-                                .datePickerStyle(.wheel)
-                                .labelsHidden()
-                        }
-                        .padding()
-
-                Button(action: sleepVM.calculateBedtime) {
-                            Text("Calculate Bedtime")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(Color.blue)
-                                .cornerRadius(10)
-                        }
-                        .padding()
-
-                        Spacer()
-
-                        Text("Recommended Bedtime:")
-                            .font(.headline)
-
-                Text(sleepVM.recommendedBedTime)
-                            .font(.title)
-
-                        Spacer()
-                    }
-                    .padding(.horizontal, 30) // Add horizontal padding to the entire VStack
-                }
+                Text("Wake Up Time")
+                    .font(.headline)
+                
+                DatePicker("", selection: $sleepVM.wakeUpTime, displayedComponents: .hourAndMinute)
+                    .datePickerStyle(.wheel)
+                    .labelsHidden()
+            }
+            .padding()
+            
+            Button(action: sleepVM.calculateBedtime) {
+                Text("Calculate Bedtime")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+            
+            Text("Recommended Bedtime:")
+                .font(.headline)
+            
+            Text(sleepVM.recommendedBedTime)
+                .font(.title)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 30) // Add horizontal padding to the entire VStack
+    }
+}
